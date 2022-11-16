5a0bb4123e6fd4aa71183e94ef5c13ca1fdf370f
2019082851 <2019082851@hanyang.ac.kr>
2022-11-16 14:32:45 +0900

    delete interrupt.h

--- Challenge_1/main.cc
+++ Challenge_1/main.cc
@@ -4,20 +4,38 @@
  *  Created on: 2022. 11. 14.
  *      Author: Chung_Seung_Yeon
  */
-
+#include <iostream>
 #include "led.h"
 #include "switch.h"
 #include "Clock.h"
 #include "IR_sensor.h"
 #include "motor.h"
-#include "interrupt.h"
-#include <iostream>
 
 
-void task(){
+
+std::function<void()> task = [](){
     printf("interrupt occurs!\n");
+};
+
+std::function<void()> TimerA2Task;
+
+void TimerA2_Init(std::function<void()> task,uint16_t period){
+        TimerA2Task = task;
+        TIMER_A2->CTL = 0x0280;
+        TIMER_A2->CCTL[0] = 0x0010;
+        TIMER_A2->CCR[0] = (period - 1);
+        TIMER_A2->EX0 = 0x0005;
+        NVIC->IP[3] = (NVIC->IP[3]&0xFFFFFF00)|0x00000040;
+        NVIC->ISER[0] = 0x00001000;
+        TIMER_A2->CTL |=0x0014;
+
 }
 
+void TA2_0_IRQHandler(void){
+        TIMER_A2->CCTL[0] &= ~0x0001;
+
+        TimerA2Task();
+}
 
 void main(void)
 {
@@ -39,10 +57,10 @@ void main(void)
     // create Motor_controller
     Motor Moter_control;
 
-    // create interrupt_handler
-    Interrupt Interrupt_handler;
-    Interrupt_handler.TimerA2_Init(&task,50000);
+    // interrupt_handler Initialization
+    TimerA2_Init(task,50000);
 
+    printf("Enter while loop\n");
 
     while(1){};
 }
